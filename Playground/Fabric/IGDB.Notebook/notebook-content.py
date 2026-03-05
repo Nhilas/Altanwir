b98f2252e3f5f3616ae8071c05f8075c1a6cc778
Nhilas <zazlotimircea@gmail.com>
2026-03-05 11:47:26 +0200

    test data notebook complete. baby's first post request lol

--- Playground/Fabric/IGDB.Notebook/notebook-content.py
+++ Playground/Fabric/IGDB.Notebook/notebook-content.py
@@ -11,10 +11,30 @@
 
 # CELL ********************
 
-# Welcome to your new notebook
-# Type here in the cell editor to add code!
+import requests
+    # this is a library that specializes in http requests, among other things
 
-# lil test hi fabric
+headers = {
+    'Client-ID': 'IGDB_CLIENT_ID_REDACTED'
+    , 'Authorization': 'Bearer IGDB_BEARER_TOKEN_REDACTED'
+}
+    # my credentials. usually you'd use variables here to keep them a secret
+
+query = 'fields name, total_rating; limit 10;'
+    # this is written in a language called apicalypse
+
+response = requests.post("https://api.igdb.com/v4/games",headers=headers,data=query)
+    # this sends a POST request to the igdb api and saves it in a variable called response
+    # the result of this request is a json object
+
+df_api = spark.createDataFrame(response.json())
+    # Create a DataFrame from the API response
+    # this is kind of like a table, if a table was an object that temporarily lives in memory
+
+display(df_api) 
+    # Display the DataFrame in a tabular format
+# df_api.printSchema() 
+    # Print the schema of the DataFrame
 
 # METADATA ********************
 

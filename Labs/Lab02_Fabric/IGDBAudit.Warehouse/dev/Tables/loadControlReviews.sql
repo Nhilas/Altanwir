e87d69401ef8db20325e5c67ab45836bbd079660
Nhilas <zazlotimircea@gmail.com>
2026-03-30 17:52:18 +0300

    created dev lakehouse and schema plus populated with data

---
+++ Labs/Lab02_Fabric/IGDBAudit.Warehouse/dev/Tables/loadControlReviews.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [dev].[loadControlReviews] (
+
+	[app_id] int NOT NULL, 
+	[run_id] varchar(max) NULL, 
+	[execution_id] varchar(max) NULL, 
+	[execution_type] varchar(30) NULL, 
+	[execution_start_time] datetime2(3) NULL, 
+	[execution_end_time] datetime2(3) NULL, 
+	[execution_duration] int NULL, 
+	[execution_status] varchar(max) NULL, 
+	[retrieved_reviews] int NULL, 
+	[first_retrieved_timestamp] bigint NULL, 
+	[last_retrieved_timestamp] bigint NULL, 
+	[last_retrieved_cursor] varchar(30) NULL, 
+	[output_path] varchar(max) NULL
+);
---
+++ Labs/Lab02_Fabric/NB_Steam_Reviews_Bronze.Notebook/notebook-content.py
@@ -0,0 +1,36 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "c683a58d-3109-458d-8cb3-da991c23a31e",
+# META       "default_lakehouse_name": "IGDBAnalytics_Dev",
+# META       "default_lakehouse_workspace_id": "d1206eb3-2259-44b8-844a-409f1a63f284",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "c683a58d-3109-458d-8cb3-da991c23a31e"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# # Parameters
+
+# CELL ********************
+
+environment = "dev"
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
